b696cf8bc44af416bc646e94dfa94423eddb9af1
Israel Guillermo <israelguillermo22@gmail.com>
2020-12-12 21:23:13 -0600

    updating basic logic to display burgers already listed on db. Added handlenbars, routes and controllers.

--- config/orm.js
+++ config/orm.js
@@ -8,6 +8,20 @@ function printQuestionMarks(num) {
   return arr.toString();
 }
 
+function objToSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 const orm = {
   selectAll: function (tableInput, cb) {
     //   ask tutor about declaring here.... var vs let vs const?
@@ -37,5 +51,20 @@ const orm = {
       cb(result);
     });
   },
-  updateOne: function () {},
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+    queryString += "SET";
+    queryString += objToSql(objColVals);
+    queryString += "WHERE ";
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
 };
+
+module.exports = orm;
